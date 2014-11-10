bc61008f7420bb2fb0987565bc7dde5e82656862
Sasha Rayshubskiy <druzhiche@gmail.com>
2014-11-09 19:03:57 -0500

    first commit

---
+++ olfactometer_debug.m
@@ -0,0 +1,71 @@
+%% 
+clear all;
+close all;
+%basepath = '/Users/sasha/Documents/Wilson lab/Data/2p_olfactometer_test_2014_11_05/';
+%basepath = 'C:\Users\WilsonLab\Desktop\Sasha\2p_olfactometer_test_2014_11_07\';
+basepath = '/Users/sasha/Documents/Wilson lab/Data/2p_olfactometer_test_2014_11_07/';
+cd(basepath)
+
+trial_types = { 'both_odor', 'left_odor', 'right_odor', 'both_air', 'left_air', 'right_air' };
+FR = 7.8;
+TPRE = 5;
+STIM = 10;
+
+for tt = 1:size(trial_types,2)
+    
+    search_path = ['*' trial_types{tt} '*'];
+    files = dir([search_path '.tif']);
+        
+    for i=1:size(files,1)
+        filename = files(i).name;
+        
+        info = imfinfo(filename);
+        num_images = numel(info);
+        for k = 1:num_images-2
+            data(tt,i,:,:,k) = double(imread(filename, k));
+        end
+    end
+    
+    avg_data{tt} = squeeze(mean(squeeze(data(tt,:,:,:,:))));
+end
+
+%%
+rois = get_rois(squeeze(data(1,:,:,:,:)));
+
+left_roi  = rois{1};
+right_roi = rois{2};
+
+%% 
+clear intens_air intens_odor;
+f = figure;
+for tt=1:3
+    tt_air = tt+3;
+    intens_odor = clicky_all_data_df_f_with_rois( squeeze(data(tt,:,:,:,:)), FR, TPRE, STIM, [basepath '/'], trial_types{tt}, rois );
+    close(gcf());
+    intens_air = clicky_all_data_df_f_with_rois( squeeze(data(tt_air,:,:,:,:)), FR, TPRE, STIM, [basepath '/'], trial_types{tt_air}, rois );
+    close(gcf());
+    
+    figure(f);
+    subplot(1,3,tt);
+    nframes = size(data,5);
+    t = [0:nframes-1]./FR;
+    
+    hold on;
+    bs_fr_end = floor(TPRE*FR);
+    left_pc = (intens_odor(:,1)-intens_air(:,1))./ intens_air(:,1);
+    left_pc_s = left_pc - (mean(left_pc(1:bs_fr_end)));    
+    
+    right_pc = (intens_odor(:,2)-intens_air(:,2))./ intens_air(:,2);
+    right_pc_s = right_pc - (mean(right_pc(1:bs_fr_end)));
+    
+    p1 = plot( t', left_pc_s, 'b' );
+    p2 = plot( t', right_pc_s, 'g' );
+    legend([p1, p2], 'Left','Right');
+    xlim([0 20]);
+    ylim([-0.1 0.35]);
+    title(['(' trial_types{tt} ' - ' trial_types{tt_air} ')/' trial_types{tt_air}],'Interpreter','none');
+    xlabel('Time (s)');
+    ylabel('% change');
+end
+
+saveas(f,[basepath '/odor_air_diff.fig']);
